9d595d064bbbc2b7a78f4284edbe06921f2e0a87
officer <notbeafraidofanything@gmail.com>
2019-01-14 14:23:43 +0000

    initial

---
+++ Scripts/AfterInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 'sudo /home/ec2-user/hello.sh'
+sudo /home/ec2-user/hello.sh
+echo '/home/ec2-user/hello.sh'
+/home/ec2-user/hello.sh
+echo 'sudo ls -la /home/ec2-user/hello.sh'
+sudo ls -la /home/ec2-user/hello.sh
+echo 'ls -la /home/ec2-user/hello.sh'
+ls -la /home/ec2-user/hello.sh
